78ca9ebac3acff08f07ea35d325029c4e34a00b4
Jothi-Ram <ramukuttytheiva@gmail.com>
2020-05-10 12:27:25 +0530

    icon&appbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,25 @@
 // I need to import a helper library 
 //from flutter to get content on the screen
-import 'package:flutter/material.dart';
 //Define a 'main' function to run when our app starts
-void main(){
 //Create a new text widget to show some text 
 //on the screen
-var app = MaterialApp(
-home:Text('Jothi'),
-);
 //take that widget and get it on the screen
 
+import 'package:flutter/material.dart';
+
+void main(){
+  var app = MaterialApp(
+    home:Scaffold(
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: (){
+          print('Jothi');
+        },
+      ),
+      appBar: AppBar(
+        title: Text('Lets see some Images!'),
+      ),
+    ),
+);
 runApp(app);
 }
---
+++ lib/src/app.dart
@@ -0,0 +1,11 @@
+//Immport flutter helper library
+import 'package:flutter/material.dart';
+//Create a class that will be our custom widget
+//This class must extend the 'StatelessWidget' base class
+class App extends StatelessWidget{
+  
+}
+
+
+//Must define a 'build' method that returns
+//the widget that *this* widget will show
