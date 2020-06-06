4754054509006fd061264889f2f306dabee8e247
Jothi-Ram <ramukuttytheiva@gmail.com>
2020-06-06 20:58:04 +0530

    ImageAPI

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,5 +1,6 @@
 //Immport flutter helper library
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' show get;
 
 class App extends StatefulWidget{
   createState(){
@@ -12,17 +13,18 @@ class App extends StatefulWidget{
 class AppState extends State<App>{
   int counter = 0;
 
+  void fetchImage() async{
+    counter++;
+    var response = await get('https://jsonplaceholder.typicode.com/photos/$counter');
+  }
+
   Widget build(context){
     return MaterialApp(
       home:Scaffold( 
         body: Text('$counter'),
         floatingActionButton: FloatingActionButton(
           child: Icon(Icons.add),
-          onPressed: (){
-            setState(() {
-            counter +=1;  
-            });
-          },
+          onPressed: fetchImage,
         ), 
         appBar: AppBar(
           title: Text('Lets see some Images!'),
