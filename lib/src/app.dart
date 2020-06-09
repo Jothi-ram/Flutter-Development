e40d8e4191b7671ce9b16556999fbf892dd1a4dc
Jothi-Ram <ramukuttytheiva@gmail.com>
2020-06-09 22:31:01 +0530

    ImageModel

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,7 +1,9 @@
 //Immport flutter helper library
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' show get;
-  
+import 'models/image_model.dart';
+import 'dart:convert';
+
 class App extends StatefulWidget{
   createState(){
     return AppState();
@@ -12,10 +14,17 @@ class App extends StatefulWidget{
 //This class must extend the 'StatelessWidget' bases class
 class AppState extends State<App>{
   int counter = 0;
+ List<ImageModel> images = [];
 
   void fetchImage() async{
     counter++;
     var response = await get('https://jsonplaceholder.typicode.com/photos/$counter');
+    var imageModel = ImageModel.fromJson(json.decode(response.body));
+    
+    setState(() {
+      images.add(imageModel);  
+    });
+    
   } 
 
   Widget build(context){
---
+++ lib/src/widegets/image_list.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import '../models/image_model.dart';
+
+clas ImageList extends  StatelessWidget{
+  
+}
